8ce5fa651e19fc2ae159477e14c025de3cdfd276
Tine-m <tinemarbjerg@gmail.com>
2023-03-14 19:09:47 +0100

    changed property file

--- src/main/java/com/example/fullstackdemo/repositories/DepartmentRepository.java
+++ src/main/java/com/example/fullstackdemo/repositories/DepartmentRepository.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @Repository
 public class DepartmentRepository {
+
   public Department findByDeptno(int deptNo) {
     try {
       Connection con = DBManager.getConnection();
